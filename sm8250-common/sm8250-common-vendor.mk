aec900bba30a36b84ed4704074a5e2f02893897b
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2022-05-14 15:45:21 -0300

    sm8250-common: Import VulkanAPI Layers from Google Pixel 5
    * Imported from dump redfin-user-12-S3B1.220318.003-8372304-release-keys

--- sm8250-common/sm8250-common-vendor.mk
+++ sm8250-common/sm8250-common-vendor.mk
@@ -735,6 +735,7 @@ PRODUCT_PACKAGES += \
     SoterService \
     TimeService \
     com.qualcomm.qti.gpudrivers.kona.api30 \
+    gpu_profiling_vulkan_layer \
     PowerOffAlarm \
     QtiTelephonyService \
     ims \
